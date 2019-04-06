cfbb46a3bf293e08f9dad32b013e4ac25b84d8d9
Derayan Bima Alamsyah <aderayanbima31@gmail.com>
2019-04-06 10:53:58 +0700

    Initial commit

---
+++ app/src/main/java/id/gits/gitsmvvmkotlin/data/source/remote/GitsRemoteDataSource.kt
@@ -0,0 +1,46 @@
+package id.gits.gitsmvvmkotlin.data.source.remote
+
+import id.gits.gitsmvvmkotlin.data.model.Movie
+import id.gits.gitsmvvmkotlin.data.source.GitsDataSource
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * Created by irfanirawansukirman on 26/01/18.
+ */
+
+
+object GitsRemoteDataSource : GitsDataSource {
+
+    private val apiService = GitsApiService.create()
+
+    override fun getMovies(callback: GitsDataSource.GetMoviesCallback) {
+        apiService.getMovies()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe({ results ->
+                    run {
+                        if (results.results!!.isNotEmpty()) {
+                            callback.onMoviesLoaded(results.results)
+                        } else {
+                            callback.onDataNotAvailable()
+                        }
+                    }
+                }, { error ->
+                    callback.onError(error.message)
+                })
+    }
+
+    override fun getMovieById(movieId: Int, callback: GitsDataSource.GetMoviesByIdCallback) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun saveMovie(movie: Movie) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun remoteMovie(isRemote: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
